630332f186cc6a1eb5d686c373fd089b6fa77564
Travis Grammer <travisjgrammer@gmail.com>
2018-06-09 00:10:30 -0400

    packer: apk update/install script

---
+++ scripts/install.sh
@@ -0,0 +1,9 @@
+set -ux
+
+# add edge repos
+cat <<EOF >> /etc/apk/repositories
+http://dl-cdn.alpinelinux.org/alpine/edge/main
+@edgecommunity http://dl-cdn.alpinelinux.org/alpine/edge/community
+EOF
+apk update -v
+
